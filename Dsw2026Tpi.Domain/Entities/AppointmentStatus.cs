4033dff31134750ac77c8b38d8810d7f138d55fc
AgustinCarabajal <enrique.carabajal@alu.frt.utn.edu.ar>
2026-08-03 11:51:17 -0300

    enum con los estados

---
+++ Dsw2026Tpi.Domain/Entities/AppointmentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Domain.Entities;
+
+internal class AppointmentStatus
+{
+    public enum AppointmentStatus
+    {
+        Booked,
+        Cancelled,
+        Attended,
+        NoShow
+    }
+}
