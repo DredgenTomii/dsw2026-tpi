9cc4f40970e95d9660197cd364ebaf41814394f7
AgustinaValverdi <agusvalverdi1@gmail.com>
2026-08-04 16:19:18 -0300

    agrego migracion para renombrar deleted en doctor

---
+++ Dsw2026Tpi.Data/Migrations/Domain/20260804191403_RenameDoctorIsActiveToDeleted.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dsw2026Tpi.Data.Migrations.Domain
+{
+    /// <inheritdoc />
+    public partial class RenameDoctorIsActiveToDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Doctors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Doctors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
